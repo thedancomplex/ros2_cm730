d03e35a176a20f043e3097f310031c556302a5c6
Sander van Dijk <sgvandijk@gmail.com>
2019-01-26 18:52:17 +0000

    Joint state publisher uses proper names

--- mx_joint_state_publisher/src/mx_joint_state_publisher.cpp
+++ mx_joint_state_publisher/src/mx_joint_state_publisher.cpp
@@ -6,14 +6,41 @@ namespace mx_joint_state_publisher
   MxJointStatePublisher::MxJointStatePublisher()
     : rclcpp::Node{"mx_joint_state_publisher"}
   {
+    auto jointNames = std::vector<std::string>{};
+    get_parameter_or("joint_names", jointNames, {
+        "base",
+        "shoulder-pitch-r",
+        "shoulder-pitch-l",
+        "shoulder-roll-r",
+        "shoulder-roll-l",
+        "elbow-r",
+        "elbow-l",
+
+        "hip-yaw-r",
+        "hip-yaw-l",
+        "hip-roll-r",
+        "hip-roll-l",
+        "hip-pitch-r",
+        "hip-pitch-l",
+        "knee-r",
+        "knee-l",
+        "ankle-pitch-r",
+        "ankle-pitch-l",
+        "ankle-roll-r",
+        "ankle-roll-l",
+
+        "head-pan",
+        "head-tilt",
+      });
+    
     jointStatePub_ = create_publisher<JointState>("joint_states");
     
     mx28InfoSub_ = create_subscription<MX28InfoArray>(
       "mx28info",
-      [this](MX28InfoArray::SharedPtr info) {
+      [=](MX28InfoArray::SharedPtr info) {
         auto jointStateMsg = std::make_shared<JointState>();
         for (auto const& mx : info->mx28s) {
-          jointStateMsg->name.push_back(std::to_string(mx.stat.id));
+          jointStateMsg->name.push_back(jointNames[mx.stat.id]);
           jointStateMsg->position.push_back(value2Rads(mx.dyna.present_position));
         }
         jointStatePub_->publish(jointStateMsg);
