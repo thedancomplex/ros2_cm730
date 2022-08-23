cd800e17ca4424d70cfc358b566f4d15b5849d8d
Daniel M. Lofaro <dan@danlofaro.com>
2022-08-22 20:28:09 -0400

    added ros2 humble install

---
+++ install_ros2_humble.sh
@@ -0,0 +1,20 @@
+locale  # check for UTF-8
+
+sudo apt update && sudo apt install locales
+sudo locale-gen en_US en_US.UTF-8
+sudo update-locale LC_ALL=en_US.UTF-8 LANG=en_US.UTF-8
+export LANG=en_US.UTF-8
+
+locale  # verify settings
+
+apt-cache policy | grep universe
+
+sudo apt install software-properties-common
+sudo add-apt-repository universe
+
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(source /etc/os-release && echo $UBUNTU_CODENAME) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
+
+sudo apt update
+
+sudo apt install ros-humble-ros-base
+#sudo apt install ros-humble-desktop
