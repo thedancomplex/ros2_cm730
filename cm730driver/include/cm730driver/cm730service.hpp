b4ad40209a1fc2499b85558edf547a0772f19ec6
Daniel M. Lofaro <dan@danlofaro.com>
2022-08-26 17:41:46 -0400

    casting out side of rclcpp::get_logger

--- cm730driver/include/cm730driver/cm730service.hpp
+++ cm730driver/include/cm730driver/cm730service.hpp
@@ -164,7 +164,8 @@ void Cm730Service<INSTR, ServiceT, Derived, CHECK_CHECKSUM>::handle(
     str <<
       "Received request: " <<
       rosidl_generator_traits::data_type<typename ServiceT::Request>();
-      RCLCPP_DEBUG(rclcpp::get_logger("cm730service"), std::__cxx11::basic_string<char>);
+
+      RCLCPP_DEBUG(std::to_string(rclcpp::get_logger("cm730service")), str.str());
       //RCLCPP_DEBUG(rclcpp::get_logger("cm730service"), str.str());
   }
 #endif
