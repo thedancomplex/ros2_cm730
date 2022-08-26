c75a1595eb72cb7e55c7679ac926e588994bd553
Daniel M. Lofaro <dan@danlofaro.com>
2022-08-26 16:31:25 -0400

    moved the files, now need to install a fresh install and test

--- install/install_ros2_humble.sh
+++ install/install_ros2_humble.sh
@@ -1,3 +1,6 @@
+HUMBLE_INSTALL_DIR=~/ros2_humble/
+DEB_VERSION=$(env -i bash -c '. /etc/os-release; echo $VERSION_CODENAME')
+
 locale  # check for UTF-8
 
 sudo apt update && sudo apt install locales
@@ -12,9 +15,74 @@ apt-cache policy | grep universe
 sudo apt install software-properties-common
 sudo add-apt-repository universe
 
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(source /etc/os-release && echo $UBUNTU_CODENAME) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
+sudo apt update && sudo apt install curl gnupg lsb-release
+sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
 
-sudo apt update
 
-sudo apt install ros-humble-ros-base
-#sudo apt install ros-humble-desktop
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(source /etc/os-release && echo $DEB_CODENAME) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
+
+sudo apt update && sudo apt install -y \
+  build-essential \
+  cmake \
+  git \
+  python3-flake8 \
+  python3-flake8-* \
+  python3-pip \
+  python3-pytest \
+  python3-pytest-cov \
+  python3-pytest-repeat \
+  python3-pytest-rerunfailures \
+  python3-rosdep \
+  python3-setuptools \
+  python3-vcstool \
+  wget
+
+sudo apt install libtinyxml2-dev
+sudo apt install libasio-dev 
+sudo apt install libacl1-dev
+sudo apt install gcc g++ cmake libacl1-dev libncurses5-dev pkg-config
+sudo apt install libeigen3-dev
+sudo apt install python3-lark
+sudo apt install python3-numpy
+sudo apt install libldap2-dev
+sudo apt install rtirq-init
+sudo apt install libbullet-dev
+sudo apt install python3-opencv
+sudo apt install libopencv-dev
+sudo apt-get install mesa-common-dev
+sudo apt install libqt5core5a
+sudo apt-get install qtbase5-dev
+sudo apt-get install qtdeclarative5-dev
+
+THE_DIR=$(pwd)
+
+cd /tmp/
+git clone https://github.com/eclipse-cyclonedds/cyclonedds.git
+cd cyclonedds
+mkdir build
+cmake ../
+make
+sudo make install
+
+cd $THE_DIR
+
+sudo systemctl mask brltty.path
+sudo systemctl mask brltty
+
+
+mkdir -p $HUMBLE_INSTALL_DIR/src
+cp ros2.repos.32bit $HUMBLE_INSTALL_DIR/
+
+cd $HUMBLE_INSTALL_DIR
+#wget https://raw.githubusercontent.com/ros2/ros2/humble/ros2.repos
+vcs import src < ros2.repos.32bit
+
+sudo apt upgrade
+
+sudo rosdep init
+rosdep update
+rosdep install --from-paths src --ignore-src -y --skip-keys "fastcdr rti-connext-dds-6.0.1 urdfdom_headers"
+
+colcon build --simlink-install --packages-skip-build-finished
+
+echo ". ~/ros2_humble/install/local_setup.bash" >> ~/.bashrc
