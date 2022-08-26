addf79b405310a4ae20f1f11fc10e6756faca771
Daniel M. Lofaro <dan@danlofaro.com>
2022-08-26 19:28:22 -0400

    now trying to use string cat instead of +

--- cm730driver/include/cm730driver/cm730service.hpp
+++ cm730driver/include/cm730driver/cm730service.hpp
@@ -166,7 +166,7 @@ void Cm730Service<INSTR, ServiceT, Derived, CHECK_CHECKSUM>::handle(
       rosidl_generator_traits::data_type<typename ServiceT::Request>();
       // removed
       //RCLCPP_DEBUG(rclcpp::get_logger("cm730service"), str.str());
-      RCLCPP_DEBUG(rclcpp::get_logger("cm730service"), str.str().c_str());
+      RCLCPP_DEBUG(  strcat(rclcpp::get_logger("cm730service"), str.str().c_str())  );
   }
 #endif
 
