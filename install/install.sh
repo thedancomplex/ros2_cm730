3221144029d08fe63e9c6e6f72f6d68d026bb479
Daniel M. Lofaro <dan@danlofaro.com>
2022-08-26 23:24:58 -0400

    seems to be working, both ros2 and cm730 install... did nto boot the robot yet, when I do I will try a fresh install

--- install/install.sh
+++ install/install.sh
@@ -22,7 +22,7 @@ InstallRos2()
   sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
 
 
-  echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(source /etc/os-release && echo $DEB_CODENAME) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
+  echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(source /etc/os-release && echo $DEB_VERSION) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
 
   sudo apt update && sudo apt install -y \
     build-essential \
